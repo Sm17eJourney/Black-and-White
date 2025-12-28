3f33f7803c274fe0593a2ed13de4b56c96f42196
Sm17eJourney <sm17e.journeyy@gmail.com>
2025-12-28 23:54:26 +0800

    Add files via upload

--- javascript/drawjavascript.js
+++ javascript/drawjavascript.js
@@ -1,69 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* document.addEventListener('DOMContentLoaded', () => {
     initSwiper(); 
-});
+}); */
+document.addEventListener('DOMContentLoaded', () => {
+    const isMobile = window.innerWidth < 991;
 
-const isMobile = window.innerWidth < 991;
+    const swiper = new Swiper('.book-swiper', {
+        slidesPerView: 1,
+        speed: 700,
+        grabCursor: true,
+        
 
-const swiper = new Swiper('.book-swiper', {
-    slidesPerView: 1,
-    speed: 700,
-    grabCursor: true,
-    
-
-    effect: isMobile ? 'slide' : 'creative',
-    creativeEffect: {
-        prev: {
-        translate: ['-100%', 0, -300],
-        rotate: [0, 0, -15],
-        shadow: true,
+        effect: isMobile ? 'slide' : 'creative',
+        creativeEffect: {
+            prev: {
+            translate: ['-100%', 0, -300],
+            rotate: [0, 0, -15],
+            shadow: true,
+            },
+            next: {
+            translate: ['100%', 0, -300],
+            rotate: [0, 0, 15],
+            shadow: true,
+            },
         },
-        next: {
-        translate: ['100%', 0, -300],
-        rotate: [0, 0, 15],
-        shadow: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+    });
 
 
 
 
-document.getElementById('firstBtn').onclick = () => {
-    swiper.slideTo(0);
-};
+    document.getElementById('firstBtn').onclick = () => {
+        swiper.slideTo(0);
+    };
 
-document.getElementById('lastBtn').onclick = () => {
-    swiper.slideTo(swiper.slides.length - 1);
-};
+    document.getElementById('lastBtn').onclick = () => {
+        swiper.slideTo(swiper.slides.length - 1);
+    };
 
-document.getElementById('prevBtn').onclick = () => {
-    swiper.slidePrev();
-};
+    document.getElementById('prevBtn').onclick = () => {
+        swiper.slidePrev();
+    };
 
-document.getElementById('nextBtn').onclick = () => {
-    swiper.slideNext();
-};
-
-
-$("#pageFld").val(swiper.realIndex + 1);
-$("#pageFld").attr("max", swiper.slides.length);
-
-swiper.on("slideChange", function () {
-  $("#pageFld").val(swiper.realIndex + 1);
-});
+    document.getElementById('nextBtn').onclick = () => {
+        swiper.slideNext();
+    };
 
 
-$("#pageFld").on("change", function () {
-  let page = parseInt($(this).val(), 10);
+    $("#pageFld").val(swiper.realIndex + 1);
+    $("#pageFld").attr("max", swiper.slides.length);
 
-  if (isNaN(page)) return;
+    swiper.on("slideChange", function () {
+    $("#pageFld").val(swiper.realIndex + 1);
+    });
 
-  page = Math.max(1, Math.min(page, swiper.slides.length));
 
-  swiper.slideTo(page - 1);
+    $("#pageFld").on("change", function () {
+    let page = parseInt($(this).val(), 10);
+
+    if (isNaN(page)) return;
+
+    page = Math.max(1, Math.min(page, swiper.slides.length));
+
+    swiper.slideTo(page - 1);
+    });
+
 });
