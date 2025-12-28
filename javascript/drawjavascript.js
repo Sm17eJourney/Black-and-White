20c2426e64e81a0b2a0fd597d90a83b78d5c642b
Sm17eJourney <sm17e.journeyy@gmail.com>
2025-12-29 01:39:56 +0800

    Add files via upload

--- javascript/drawjavascript.js
+++ javascript/drawjavascript.js
@@ -1,25 +1,22 @@
-const isMobile = window.innerWidth < 991;
+const isMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent);
 
 const swiper = new Swiper('.book-swiper', {
     slidesPerView: 1,
     speed: 700,
     grabCursor: true,
-    
-
     effect: isMobile ? 'slide' : 'creative',
     creativeEffect: {
         prev: {
-        translate: ['-100%', 0, -300],
-        rotate: [0, 0, -15],
-        shadow: true,
+            translate: ['-100%', 0, -300],
+            rotate: [0, 0, -15],
+            shadow: true,
         },
         next: {
-        translate: ['100%', 0, -300],
-        rotate: [0, 0, 15],
-        shadow: true,
+            translate: ['100%', 0, -300],
+            rotate: [0, 0, 15],
+            shadow: true,
         },
     },
-
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -62,4 +59,17 @@ $("#pageFld").on("change", function () {
   page = Math.max(1, Math.min(page, swiper.slides.length));
 
   swiper.slideTo(page - 1);
+});
+
+let isSliding = false;
+
+$("#pageFld").on("change", function () {
+    if (isSliding) return;
+    let page = parseInt($(this).val(), 10);
+    if (isNaN(page)) return;
+
+    page = Math.max(1, Math.min(page, swiper.slides.length));
+    isSliding = true;
+    swiper.slideTo(page - 1, 700, false);
+    setTimeout(() => { isSliding = false }, 800);
 });
